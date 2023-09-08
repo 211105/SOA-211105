0f425c7e089caeff69d82bf95dada59f35655465
Luis Antonio Martinez Marroquin <LuisMartinezMA12@gmail.com>
2023-09-07 19:26:57 -0600

    Se modificaron las respuestas http

--- src/main/java/com/example/demo/Controller/personController.java
+++ src/main/java/com/example/demo/Controller/personController.java
@@ -2,6 +2,7 @@ package com.example.demo.Controller;
 
 import com.example.demo.model.person;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import com.example.demo.repository.personRepository;
@@ -10,12 +11,11 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.server.ResponseStatusException;
 
-
-
+import java.net.URI;
+import java.net.URISyntaxException;
 import java.util.List;
 import java.util.Map;
 import java.util.Optional;
@@ -40,23 +40,40 @@ public class personController {
 
     //Listar todas
     @GetMapping("/allpersons")
-    public List<person> allPersons(){
-        return repository.findAll();
+    public ResponseEntity<List<person>> getAllPersons() {
+        List<person> persons = repository.findAll();
+        if (persons.isEmpty()) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "No se encontraron datos");
+        }
+        return ResponseEntity.ok(persons);
     }
 
     //traer por un ID dad
-    @GetMapping("/persons/{id}")
+    @GetMapping("/{id}")
     public ResponseEntity<person> getPersonById(@PathVariable Long id) {
         Optional<person> personOptional = repository.findById(id);
 
-        return personOptional.map(ResponseEntity::ok).orElseGet(() -> ResponseEntity.notFound().build());
+        if (personOptional.isPresent()) {
+            return ResponseEntity.ok(personOptional.get());
+        } else {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Persona no encontrada con ID: " + id);
+        }
     }
     //agregar nuevo usuario
+    /**
+     * Crea una nueva persona.
+     *
+     * @param person Datos de la persona a crear.
+     * @return ResponseEntity con la persona creada y el código de estado 201 (CREATED).
+     * @throws URISyntaxException Si hay un problema con la URI de respuesta.
+     */
     @PostMapping("/person")
-    public person createPerson(@RequestBody person person) {
-        return repository.save(person);
+    public ResponseEntity<person> createPerson(@Valid @RequestBody person person) throws URISyntaxException {
+        person createdPerson = repository.save(person);
+        //ResponseEntity 201
+        return ResponseEntity.created(new URI("/api/person/" + createdPerson.getId()))
+                .body(createdPerson);
     }
-
     //modificar el estado
     @PutMapping("/persons/{id}/update-state")
     public ResponseEntity<String> updatePersonState(@PathVariable Long id, @RequestBody Map<String, String> updateData) {
@@ -65,14 +82,24 @@ public class personController {
         if (personOptional.isPresent()) {
             person person = personOptional.get();
 
-            // Validación para asegurarse de que solo se modifique el campo de estado
+            // Verificar si la solicitud contiene el campo 'estado' y si no es nulo ni una cadena vacía
             if (updateData.containsKey("estado")) {
                 String newEstado = updateData.get("estado");
-                person.setEstado(newEstado);
 
-                repository.save(person); // Guardar los cambios en la base de datos
+                if (newEstado != null && !newEstado.trim().isEmpty()) {
+                    // Validar que el campo 'estado' no sea igual en todos sus caracteres (por ejemplo, "111")
+                    if (!newEstado.matches("^\\d+$")) {
+                        person.setEstado(newEstado);
 
-                return ResponseEntity.ok("Estado actualizado correctamente.");
+                        repository.save(person); // Guardar los cambios en la base de datos
+
+                        return ResponseEntity.ok("Estado actualizado correctamente.");
+                    } else {
+                        return ResponseEntity.badRequest().body("El campo 'estado' no puede consistir solo en números iguales.");
+                    }
+                } else {
+                    return ResponseEntity.badRequest().body("El campo 'estado' no puede estar vacío ni ser nulo.");
+                }
             } else {
                 return ResponseEntity.badRequest().body("El cuerpo de la solicitud debe contener el campo 'estado'.");
             }
@@ -80,6 +107,18 @@ public class personController {
             return ResponseEntity.notFound().build();
         }
     }
+
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<String> deletePersonById(@PathVariable Long id) {
+        if (repository.existsById(id)) {
+            repository.deleteById(id);
+            return ResponseEntity.ok("Persona eliminada con éxito.");
+        } else {
+            return ResponseEntity.ok("No se pudo encontrar una persona con ID: " + id);
+        }
+    }
+
 
 
 }
